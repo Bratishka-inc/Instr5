65e85e67c84c263282ccca2b692a0c8b0056f640
et.bogdanov <begor3977@gmail.com>
2025-12-02 02:42:12 +0300

    commit 5

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    // 4) Добавил простой расчёт
-    int a = 2 + 3; // небольшое арифметическое действие
-        System.out.println("2 + 3 = " + a);
+    // 5) Добавил вывод текущего времени
+    java.time.LocalTime time = java.time.LocalTime.now(); // получил текущее время
+        System.out.println("Текущее время: " + time); // вывел его на экран
 }
